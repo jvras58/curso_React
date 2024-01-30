810e5450ed26110af797eaa3ceaab22a6f409f0a
jvras <48683351+jvras58@users.noreply.github.com>
2024-01-29 23:34:04 -0300

    fix correção no app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Empresa from "./components/pages/Empresa";
 import Contato from "./components/pages/Contato";
 
 function App() {
-
-
   return (
-  <Router>
-    <ul>
-      <li>
-        <Link to = "/">Home</Link>
-      </li>
-      <li>
-        <Link to = "/empresas">Empresa</Link>
-      </li>
-      <li>
-        <Link to = "/contato">Contato</Link>
-      </li>
-    </ul>
-    <Route path='/' exact element={<Home />}></Route>
-    <Route path='/empresas' exact element={<Empresa />}></Route>
-    <Route path='/contato' exact element={<Contato />}></Route>
-  </Router>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/empresas">Empresa</Link>
+        </li>
+        <li>
+          <Link to="/contato">Contato</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/empresas' element={<Empresa />} />
+        <Route path='/contato' element={<Contato />} />
+      </Routes>
+    </Router>
   );
 }
 
